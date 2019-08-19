2025b5edf155195c5b7ff30b651bc43ef79c2541
kargo <karenrgo@gmail.com>
2019-08-19 16:25:44 -0700

    fixed merge conflicts

--- src/containers/SavedPlaylists.js
+++ src/containers/SavedPlaylists.js
@@ -3,52 +3,33 @@ import '../App.css';
 import Song from '../components/Song'
 import PlaylistCard from '../components/PlaylistCard'
 
-const PLAYLISTS_URL ='http://localhost:8888/playlists'
+const PLAYLISTS_URL = 'http://localhost:8888/playlists'
 
 class SavedPlaylists extends React.Component {
 
-<<<<<<< HEAD
+
+
+
   render() {
-    console.log(this.props)
     return (
       <div>
-        <h1>this be a list of saved Playlists</h1>
+        <h1>Saved Playlists</h1>
         {
           this.props.allPlaylists.map(playlist => {
-            return <PlaylistCard
-              key={playlist.id}
-              playlist={playlist}
-              handleShowPlaylist={this.props.handleShowPlaylist}
-            />
+            if (!playlist.name.includes('Library')) {
+              return <PlaylistCard
+                isClicked={this.props.isClicked}
+                onPlaylistClick={this.props.onPlaylistClick}
+                key={playlist.id}
+                playlist={playlist}
+                playlistSongs={this.props.playlistSongs}
+              />
+            }
           })
         }
       </div>
     );
   }
-=======
-
-
-
-    render() {
-        return (
-            <div>
-            <h1>Saved Playlists</h1>
-          {
-            this.props.allPlaylists.map(playlist => {
-              if (!playlist.name.includes('Library')) {
-              return <PlaylistCard
-              isClicked={this.props.isClicked}
-              onPlaylistClick={this.props.onPlaylistClick}
-              key={playlist.id}
-              playlist={playlist}
-              playlistSongs={this.props.playlistSongs}
-            />}
-            })
-          }
-          </div>
-        );
-    }
->>>>>>> master
 }
 
 export default SavedPlaylists;
