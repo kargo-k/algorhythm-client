fafd9f965e8427af1272fdcefef8b6f6cf4a3e5f
kargo <karenrgo@gmail.com>
2019-08-22 09:12:08 -0700

    css tweaks

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className="App bg" >
+        <div className="App" >
           <Route exact path='/' component={Welcome} />
           <Route exact path='/user' component={User} />
           <Route exact path='/error' component={ErrorMsg} />
