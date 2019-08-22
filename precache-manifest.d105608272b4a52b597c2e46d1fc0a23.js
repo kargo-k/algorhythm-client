4fd3826d5801d87868cdc3c06158cd9a7b59b278
kargo <karenrgo@gmail.com>
2019-08-22 15:52:56 -0700

    Updates

---
+++ precache-manifest.d105608272b4a52b597c2e46d1fc0a23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2c5b74515bbbe9ee1db0bc66931c66",
+    "url": "/algorhythm-client/index.html"
+  },
+  {
+    "revision": "d9e3402e9c7036b638d8",
+    "url": "/algorhythm-client/static/css/main.458d9aca.chunk.css"
+  },
+  {
+    "revision": "c7fa82463b7acd99909e",
+    "url": "/algorhythm-client/static/js/2.32c47737.chunk.js"
+  },
+  {
+    "revision": "d9e3402e9c7036b638d8",
+    "url": "/algorhythm-client/static/js/main.df5ff305.chunk.js"
+  },
+  {
+    "revision": "b0929a684c1ab6e606e6",
+    "url": "/algorhythm-client/static/js/runtime~main.231b018b.js"
+  },
+  {
+    "revision": "cc4e2784f47909e55d940c014cbef882",
+    "url": "/algorhythm-client/static/media/Spotify_Icon_RGB_White.cc4e2784.png"
+  },
+  {
+    "revision": "936cd709484080ce0b747ad742969cf8",
+    "url": "/algorhythm-client/static/media/headphones.936cd709.jpg"
+  }
+]);
